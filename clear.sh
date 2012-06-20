013a7d68d1e9ed880909e4afef2f073c9d5aa73f
Vladimir Grinenko <Vladimir Grinenko tadatuta@yandex-team.ru>
2012-06-20 06:01:58 +0300

    gh-pages

---
+++ clear.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm index.html
+rm index.css
+rm index.ie.css
+rm index.js
---
+++ generate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+bem make
+cp pages-desktop/index/index.html .
+cp pages-desktop/index/index.css .
+cp pages-desktop/index/index.ie.css .
+cp pages-desktop/index/index.js .
