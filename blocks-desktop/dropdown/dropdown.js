c9959a12a947382d858c371791fd79d284ca00c7
Vladimir Grinenko <Vladimir Grinenko tadatuta@yandex-team.ru>
2012-07-15 05:26:11 +0300

    dashboard

--- blocks-desktop/dropdown/dropdown.js
+++ blocks-desktop/dropdown/dropdown.js
@@ -1,14 +1,47 @@
-BEM.DOM.decl('dropdown',
-{
-    onSetMod: {
-        'js': function() {
-            this.bindTo('switcher', 'click', function() {
-                this
-                    .toggleMod('state', 'shown')
-                    .findBlockInside('dropdown').toggleMod('visibility', 'visible');
+(function($) {
+    BEM.DOM.decl('dropdown',
+    {
+        onSetMod: {
+            'js': function() {
+                this.bindTo('switcher', 'click', function() {
+                    this.toggleMod('state', 'shown')
+                });
+            },
+            'state' : {
+                'shown': function() {
+                    this.findBlockInside('dropdown').toggleMod('visibility', 'visible');
+                    this.trigger('shown');
 
-                this.trigger('click');
-            });
+                    var keyDownEvent = $.browser.opera ? 'keypress' : 'keydown';
+                    this.afterCurrentEvent(function() {
+                        this
+                            .bindToDoc('leftclick', function(e) {
+                                this.containsDomElem($(e.target)) || this._onOutClick(e);
+                            })
+                            .bindToDoc(keyDownEvent, function(e) {
+                                // close if Escape pressed
+                                e.keyCode == 27 && this.hide();
+                            });
+                    });
+                },
+                '': function() {
+                    this.trigger('hide');
+
+                    return this.unbindFromDoc('leftclick keypress')
+                }
+            }
+        },
+        show: function() {
+            this.setMod('state', 'shown');
+        },
+        hide: function() {
+            this.delMod('state');
+        },
+        _onOutClick : function(domEvent) {
+            var e = $.Event('outside-click');
+            this.trigger(e, { domEvent : domEvent });
+            e.isDefaultPrevented() || this.hide();
         }
-    }
-});
+
+    });
+})(jQuery);
